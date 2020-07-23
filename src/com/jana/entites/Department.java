d62e3fe9c3f308bf2880978e4ebf84572ea9c925
Janardhan <Janardhan@192.168.2.4>
2020-07-23 16:30:48 +0530

    removed the unwanted code

--- src/com/jana/entites/Department.java
+++ src/com/jana/entites/Department.java
@@ -15,86 +15,70 @@ import javax.persistence.Table;
 import com.sun.istack.NotNull;
 
 @Entity
-@Table(name="pjr_department")
-public class Department implements Serializable{
+@Table(name = "pjr_department")
+public class Department implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	@Id
-	@Column(name="department_no")
-	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name = "department_no")
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private int departmentNo;
-	@Column(name="department_name")
+	@Column(name = "department_name")
 	private String departmentName;
-	
-	@Column(name="success_Growth_rt_percent")
-	private double successGrowthRate;
-	
-	private String status;
-	
-	@NotNull
-	@OneToMany(mappedBy="department",fetch=FetchType.LAZY)
-	
-	private Set<Employee> employees;
 
+	@Column(name = "success_Growth_rt_percent")
+	private double successGrowthRate;
+
+	private String status;
+
+	@NotNull
+	@OneToMany(mappedBy = "department", fetch = FetchType.LAZY)
+
+	private Set<Employee> employees;
 
 	public int getDepartmentNo() {
 		return departmentNo;
 	}
 
-
 	public void setDepartmentNo(int departmentNo) {
 		this.departmentNo = departmentNo;
 	}
-
 
 	public String getDepartmentName() {
 		return departmentName;
 	}
 
-
 	public void setDepartmentName(String departmentName) {
 		this.departmentName = departmentName;
 	}
-
-
-	
 
 	public Set<Employee> getEmployees() {
 		return employees;
 	}
 
-
 	public void setEmployees(Set<Employee> employees) {
 		this.employees = employees;
 	}
-
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
 
-
-	
-
 	public double getSuccessGrowthRate() {
 		return successGrowthRate;
 	}
-
 
 	public void setSuccessGrowthRate(double successGrowthRate) {
 		this.successGrowthRate = successGrowthRate;
 	}
 
-
 	public String getStatus() {
 		return status;
 	}
 
-
 	public void setStatus(String status) {
 		this.status = status;
 	}
-
 
 	@Override
 	public String toString() {
@@ -102,10 +86,4 @@ public class Department implements Serializable{
 				+ ", successGrowthRate=" + successGrowthRate + ", status=" + status + ", employees=" + employees + "]";
 	}
 
-
-	
-
-
-
-	
 }
